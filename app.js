f900b98ac942d148d4d8f47f4b5cc84ee466c785
spdolan76 <spdolan76@gmail.com>
2026-05-16 15:29:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebase
 
 // !!! PASTE YOUR COPIED CONFIG OBJECT DIRECTLY HERE FROM FIREBASE CONSOLE !!!
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-    messagingSenderId: "...",
-    appId: "..."
+  apiKey: "AIzaSyCidvULMz_g32saznq15q3lJrtnufd_xIo",
+  authDomain: "golfleagueskins.firebaseapp.com",
+  projectId: "golfleagueskins",
+  storageBucket: "golfleagueskins.firebasestorage.app",
+  messagingSenderId: "1079188950398",
+  appId: "1:1079188950398:web:eeda309172314e79eb772f",
+  measurementId: "G-RCWCWS954E"
 };
 
 // Initialize Cloud Connections
