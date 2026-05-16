5552bc83863628fa257363671a39f70bf55968cf
spdolan76 <spdolan76@gmail.com>
2026-05-16 17:20:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,15 +9,27 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // !!! PASTE YOUR COPIED CONFIG OBJECT DIRECTLY HERE FROM FIREBASE CONSOLE !!!
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-    messagingSenderId: "...",
-    appId: "..."
-};
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCidvULMz_g32saznq15q3lJrtnufd_xIo",
+    authDomain: "golfleagueskins.firebaseapp.com",
+    projectId: "golfleagueskins",
+    storageBucket: "golfleagueskins.firebasestorage.app",
+    messagingSenderId: "1079188950398",
+    appId: "1:1079188950398:web:eeda309172314e79eb772f",
+    measurementId: "G-RCWCWS954E"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 // Initialize Cloud Connections
 const app = initializeApp(firebaseConfig);
